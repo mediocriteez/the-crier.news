2bb047ccbb72fe79c579061acdaa9b9ab75d8eec
Jacob <jacobwintr@gmail.com>
2026-03-08 13:26:21 -0400

    revised header logo link url

--- src/app/Header/index.js
+++ src/app/Header/index.js
@@ -5,7 +5,7 @@ import LogoSVG from './LogoSVG'
 const Header = ({}) => {
     return(
         <header className={css.root}>
-            <Link href={process.env.NEXT_PUBLIC_ROOT_URL} className={css.logo}>
+            <Link href="/" className={css.logo}>
                 <LogoSVG />
             </Link>
             <p className={css.title}>The Crier</p>
